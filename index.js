b21fddce3bee99e148592aa24cc20bb2949754f8
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-14 15:57:31 -0400

    final copy

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+const getMain = document.querySelector('main');
+ 
+const removeThis = document.querySelector('main#main');
+
+getMain.remove(removeThis);
+;
+let newHeader = document.createElement('h1'); newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Saba is the champion"
+
+
+
+
+
+
